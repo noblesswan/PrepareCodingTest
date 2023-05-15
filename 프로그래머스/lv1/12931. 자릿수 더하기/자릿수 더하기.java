dceab75fc57c9146ce55b3f0715556fa119b1672
noblesswan <81548882+noblesswan@users.noreply.github.com>
2023-05-15 11:50:00 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.15 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,13 +3,9 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
- 
-        // ex) 4 / 10 = 0.4 → 0 ( int )
-        while (n > 0) {
-            // %를 이용하여 한자리씩 잘라낸다.
-            answer = answer + (n % 10);
-            // 10을 나누어 int이니 소숫점 이하로 잘린다.
-            n = n / 10;
+        String[] array = String.valueOf(n).split("");
+        for(String s : array){
+            answer += Integer.parseInt(s);
         }
         return answer;
     }

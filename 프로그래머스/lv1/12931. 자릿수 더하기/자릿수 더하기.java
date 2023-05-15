c7df5ac02837b36347522233d195afecdd4bc15f
noblesswan <81548882+noblesswan@users.noreply.github.com>
2023-05-15 16:03:15 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.12 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/lv1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -3,10 +3,11 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String[] array = String.valueOf(n).split("");
-        for(String s : array){
-            answer += Integer.parseInt(s);
-        }
+        
+        String[] arr = String.valueOf(n).split("");  // 배열의 타입을 String[]으로 변경해야 합니다.
+        for(int i=0; i<arr.length; i++){
+            answer += Integer.parseInt(arr[i]);  // 배열 요소를 정수로 변환하여 answer에 더해줍니다.
+        }        
         return answer;
     }
 }

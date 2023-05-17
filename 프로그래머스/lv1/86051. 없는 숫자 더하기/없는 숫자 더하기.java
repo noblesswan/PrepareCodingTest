a4c98b97895660d5ca868e1edfad2c257913a200
noblesswan <81548882+noblesswan@users.noreply.github.com>
2023-05-17 15:13:17 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int[] numbers) {
+        boolean[] check = new boolean[10];
+
+        for (int i = 0; i <numbers.length ; i++) {
+            check[numbers[i]] = true;
+        }
+
+        int answer = 0;
+
+        for (int i = 0; i <check.length ; i++) {
+            if(check[i]==false) {
+               answer += i;
+            }
+        }
+        return answer;
+    }
+}

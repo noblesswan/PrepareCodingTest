1a0e42e49e89531cc21b036700dcd79014ea5e9a
noblesswan <81548882+noblesswan@users.noreply.github.com>
2023-05-17 16:18:38 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.29 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int left, int right) {
+        int sum =0;
+        
+        for(int i=left; i<=right; i++) {
+        	if(Math.sqrt(i)==Math.ceil(Math.sqrt(i))) {
+        		sum -= i;
+        	} else {
+        		sum += i;
+        	}
+        }
+        return sum;
+    }
+}

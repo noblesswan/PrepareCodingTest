6b6dcdce3d1d15bec1f28a8d74f9691288f866b1
noblesswan <81548882+noblesswan@users.noreply.github.com>
2023-05-25 16:28:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.46 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,50 +1,58 @@
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.awt.Point;
 
 class Solution {
-    private static final int[] dx = {-1, 1, 0, 0}; // 상하좌우 방향
-    private static final int[] dy = {0, 0, -1, 1};
 
-    public int solution(int[][] maps) {
-        int n = maps.length; // 행의 개수
-        int m = maps[0].length; // 열의 개수
+    public static int solution(int[][] maps) {
+        int X = maps[0].length;
+        int Y = maps.length;
+        boolean[][] visited = new boolean[Y][X];
+        Queue<Point> q = new LinkedList<Point>();
+        int x = 0;
+        int y = 0;
+        int size = 0;
+        int cnt = 0;
+        Point p = new Point();
+        q.add(new Point(Y-1,X-1));
 
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0, 0, 1)); // 시작 위치를 큐에 추가
-        maps[0][0] = 0; // 방문한 위치는 벽으로 표시하여 다시 방문하지 않도록 함
-
-        while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            int x = current.x;
-            int y = current.y;
-            int count = current.count;
-
-            if (x == n - 1 && y == m - 1) {
-                return count; // 상대 팀 진영에 도착한 경우 최단 경로의 칸 수를 반환
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-
-                if (nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1) {
-                    queue.offer(new Node(nx, ny, count + 1)); // 다음 위치를 큐에 추가
-                    maps[nx][ny] = 0; // 방문한 위치는 벽으로 표시하여 다시 방문하지 않도록 함
+        while(q.isEmpty()==false) {
+            size = q.size();
+            cnt++;
+            for(int i=0;i<size;i++)
+            {
+                p = q.peek();
+                x = p.y;
+                y = p.x;
+                q.remove();
+                if(visited[y][x]==true)
+                    continue;
+                maps[y][x] = 0;
+                visited[y][x] = true;
+                if(x==0 && y==0) {
+                    return cnt;
                 }
+
+                if(x-1>-1 && maps[y][x-1]==1) { //왼쪽 한칸
+                    q.add(new Point(y,x-1));
+                }
+
+                if(x+1<X && maps[y][x+1]==1) { //오른쪽 한칸
+                    q.add(new Point(y,x+1));
+                }
+
+                if(y-1>-1 && maps[y-1][x]==1) { //위쪽 한칸
+                    q.add(new Point(y-1,x));
+                }
+
+                if(y+1<Y && maps[y+1][x]==1) { //아래쪽 한칸
+                    q.add(new Point(y+1,x));
+                }
+
             }
+
         }
 
-        return -1; // 상대 팀 진영에 도착할 수 없는 경우
-    }
-
-    class Node {
-        int x;
-        int y;
-        int count;
-
-        public Node(int x, int y, int count) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-        }
+        return -1;
     }
 }
